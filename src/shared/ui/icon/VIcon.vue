ef7fbf5e8b32bbbe65a202660946fbde88631488
dyakubovskiy <d.yakubovskiy@dshub.tech>
2025-02-23 20:25:21 +0300

    added VIcon

---
+++ src/shared/ui/icon/VIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg
+    class="icon"
+    :class="[iconColor, iconSize]">
+    <use v-bind="{ 'xlink:href': `/sprite.svg#${iconId}` }" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+type IconColor = 'default' | 'contrast'
+type IconSize = 'small' | 'medium' | 'large'
+
+export interface IconProps {
+  iconId: string
+  iconColor?: IconColor
+  iconSize?: IconSize
+}
+
+const { iconColor = 'default', iconSize = 'medium' } = defineProps<IconProps>()
+</script>
+
+<style scoped lang="scss">
+.icon {
+  vertical-align: middle;
+  display: inline-block;
+  transition: all 0.3s ease;
+  flex-shrink: 0;
+
+  &.default {
+    color: var(--icon);
+  }
+
+  &.contrast {
+    color: var(--contrast);
+  }
+
+  &.small {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
+
+  &.medium {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+
+  &.large {
+    width: 3.2rem;
+    height: 3.2rem;
+  }
+}
+</style>
---
+++ src/shared/ui/icon/index.ts
@@ -0,0 +1,7 @@
+import type { IconProps } from './VIcon.vue'
+
+import VIcon from './VIcon.vue'
+
+export type { IconProps }
+
+export { VIcon }
