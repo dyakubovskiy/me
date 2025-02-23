f6307c5b75416cfa6eed21563c278ee200aa3f2b
dyakubovskiy <d.yakubovskiy@dshub.tech>
2025-02-23 20:25:33 +0300

    add ButtonIcon

---
+++ src/shared/ui/buttons/ButtonIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    type="button"
+    class="buttonIcon"
+    @click="$emit('click')">
+    <VIcon
+      :iconId
+      :iconSize
+      :iconColor />
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { IconProps } from '../icon'
+
+import { VIcon } from '../icon'
+
+type Props = IconProps
+
+defineProps<Props>()
+defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+
+<style scoped lang="scss">
+.buttonIcon {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+}
+</style>
