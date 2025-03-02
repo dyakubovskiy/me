e3ff0537a1e719bfd35066c3cc0f56506cc36725
dyakubovskiy <d.yakubovskiy@dshub.tech>
2025-03-02 10:14:34 +0300

    updated header

--- src/app/layout/VHeader.vue
+++ src/app/layout/VHeader.vue
@@ -7,17 +7,26 @@
         <VIcon iconId="terminal" />
         dyakubovskiy
       </LocalLink>
-      <ButtonIcon
-        iconId="sun"
-        iconSize="small"
-        iconColor="contrast"
-        class="themeButton"
-        @click="toggleDark" />
+      <Transition name="switch">
+        <ButtonIcon
+          :key="themeIcon"
+          :iconId="themeIcon"
+          iconSize="small"
+          iconColor="contrast"
+          :style="{
+            background: `var(--${themeBackground})`
+          }"
+          class="themeButton"
+          @click="toggleDark" />
+      </Transition>
     </header>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Ref } from 'vue'
+
+import { computed } from 'vue'
 import { useDark, useToggle } from '@vueuse/core'
 import { VIcon } from '@/shared/ui/icon'
 import { LocalLink } from '@/shared/ui/link'
@@ -25,6 +34,9 @@ import { ButtonIcon } from '@/shared/ui/buttons'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+
+const themeIcon: Ref<string> = computed(() => (isDark.value ? 'sun' : 'moon'))
+const themeBackground: Ref<string> = computed(() => (isDark.value ? 'orange' : 'purple'))
 </script>
 
 <style scoped lang="scss">
@@ -34,9 +46,12 @@ const toggleDark = useToggle(isDark)
   position: sticky;
   top: 0;
   backdrop-filter: blur(1rem);
+  z-index: 10;
+  background: var(--header-background);
 }
 
 .header {
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -64,11 +79,28 @@ const toggleDark = useToggle(isDark)
   width: 4rem;
   height: 4rem;
   border-radius: 8px;
-  background-color: #fbd38d;
 }
 
 .icon {
   width: 2rem;
   height: 2rem;
+}
+
+.switch-enter-active,
+.switch-leave-active {
+  transition: all 0.5s ease;
+  transform-origin: top center;
+}
+
+.switch-enter-from {
+  opacity: 0;
+  transform: translateY(-300%);
+}
+
+.switch-leave-to {
+  opacity: 0;
+  position: absolute;
+  right: 0.8rem;
+  transform: translateY(100%);
 }
 </style>
