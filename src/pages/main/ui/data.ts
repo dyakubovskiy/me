703a7a6c3f3c462a4a5a73fd87b3b80c11d488c5
dyakubovskiy <d.yakubovskiy@dshub.tech>
2025-03-02 10:17:07 +0300

    added home page

---
+++ src/pages/main/ui/data.ts
@@ -0,0 +1,42 @@
+const greetings = 'Привет, я веб-разработчик из России!'
+
+const developer = 'Дмитрий Якубовский'
+const profession = 'Web developer (Frontend (Vue) / Basic Backend)'
+
+const about = `
+    Меня зовут Дмитрий, я фронтенд-разработчик из Тулы, Россия.\n Мой путь в IT начался с позиции оператора чата технической поддержки, а затем я увлекся веб-разработкой. Сейчас я специализируюсь на фронтенде, но также имею базовые знания в бэкенде.
+    Мне нравится создавать изящный и красивый код, особенно в сложных и высоконагруженных системах. В свободное время я реализую пет-проекты с интересными идеями, такие как игры (например, Пакмен) и менеджеры паролей.
+`
+
+const bio = [
+  {
+    date: '1997',
+    present: false,
+    content: 'Родился в Горловке, Украина'
+  },
+  {
+    date: '2021',
+    present: true,
+    content: 'Работа в Scloud'
+  },
+  {
+    date: '2024',
+    present: false,
+    content: 'Выступил с докладом на конференцию "Старт в IT"'
+  }
+]
+
+const socials = [
+  {
+    name: '@dyakubovskiy',
+    iconId: 'github',
+    link: 'https://github.com/dyakubovskiy'
+  },
+  {
+    name: '@dev_cube',
+    iconId: 'telegram',
+    link: 'https://t.me/dev_cube'
+  }
+]
+
+export { greetings, developer, profession, about, bio, socials }
